83835e32a3676f96b78bd9fa23879a474404f295
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-11-07 11:23:47 +0100

    eslint-disable-next-line

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ const Header = () => {
   return (
     <header className="header">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        {/*eslint-disable-next-line*/}
         <a className="navbar-brand" href="#">Portfolio</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
