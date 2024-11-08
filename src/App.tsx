cfe6c09ce4bf13a81b25cd8e60836b6da6a98398
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-11-08 11:03:43 +0100

    Refactor App component to remove unused imports and switch to SCSS styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import Header from './components/Header.tsx';
-import Footer from './components/Footer.tsx';
 import About from './components/About.tsx';
-import Projects from './components/Projects.tsx';
-import Contact from './components/Contact.tsx';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <About />
-      <Projects />
-      <Contact />
-      <Footer />
     </div>
   );
 }
