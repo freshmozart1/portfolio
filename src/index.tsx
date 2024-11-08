fdc940c39882573210383b66f2186a09f39ac050
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-11-08 11:04:38 +0100

    Add custom container class to root element in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.scss';
 
+document.getElementById('root')?.classList.add('custom-container');
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
