071c3950c9c24a95bf798d6915b8d2814e82184e
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-11-11 10:03:29 +0100

    Removed everythin because I again decided to remake everything.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
 import React from 'react';
-import About from './components/About.tsx';
 import './App.scss';
-import Project from './components/Project.tsx';
-import TweHeaderPath from './assets/twe_header.png';
-import Navigation from './components/Navigation.tsx';
-import Contact from './components/Contact.tsx';
+import 'bootstrap/js/index.umd.js';
 
 function App() {
   return (
-    <div className="App">
-      <About />
-      <Project className='Twe_project' projectImagePath={TweHeaderPath} projectName='Talk With Everyone'>
-        &gt; <b>Chat application</b> for Android and iOS<br /><br />
-        &gt; Google <b>Firebase</b><br />
-        &gt; Google Firestore<br />
-        &gt; Google Cloud Storage<br />
-        &gt; <b>React Native</b><br />
-        &gt; react-native-gifted-chat<br />
-        &gt; react-native-maps
-      </Project>
-      <Contact />
-      <Navigation />
+    <div>
     </div>
   );
 }
