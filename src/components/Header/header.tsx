ed74f2fd7b7e56fb4c5c7392fb0f536c3c8efc04
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-12-01 16:39:58 +0100

    Refactor Header component styles and update SVG icons for improved accessibility

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -31,12 +31,12 @@ export default function Header({ currentPage, setCurrentPage }: { currentPage: {
               setSubMenuItems(undefined);
             }, 500)
           }}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+            <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
               <path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z" />
             </svg>
           </button>
           <button onClick={e => toggleMenu()}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+            <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
               <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
             </svg>
           </button>
