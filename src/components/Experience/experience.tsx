6b599cf5a5db81c7e981539f744b91d9cdef3958
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-12-14 12:09:59 +0100

    Refactor Experience component layout and add sticky header functionality

--- src/components/Experience/experience.tsx
+++ src/components/Experience/experience.tsx
@@ -1,33 +1,46 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "./experience.scss";
 
 export default function Experience() {
+    const header = useRef<HTMLHeadingElement>(null);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (header.current) {
+                if ((header.current.getBoundingClientRect()).top < 1) {
+                    header.current.style.fontSize = "1rem";
+                } else {
+                    header.current.style.fontSize = "3rem";
+                }
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return (<div className="experienceContainer">
-        <h1>Experience</h1>
-        <table>
-            <tbody>
-                <tr>
-                    <td>05/24 - Present</td>
-                    <td>
-                        <b>CareerFoundry, online</b><br />
-                        Full-Stack Web Development Course
-                    </td>
-                </tr>
-                <tr>
-                    <td>10/19 - 02/24</td>
-                    <td>
-                        <b>MOIA Operations Germany GmbH, Hamburg</b><br />
-                        Bus driver with up to 40 different customers per day (kids, elderlies, handicapped)
-                    </td>
-                </tr>
-                <tr>
-                    <td>11/18 - 06/19</td>
-                    <td>
-                        <b>webförsterei, Hamburg</b><br />
-                        Frontend-Developer as part of small teams with customers ranging from small local companies to big international companies
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <h1 ref={header}>Experience</h1>
+        <section className="jobContainer">
+            <h2>CareerFoundry</h2>
+            <p>05/24 - Present</p>
+            <p>online</p>
+            <p>Full-Stack Web Development Course</p>
+        </section>
+        <section className="jobContainer">
+            <h2>MOIA Operations Germany GmbH</h2>
+            <p>10/19 - 02/24</p>
+            <p>Hamburg</p>
+            <p>Bus driver with up to 40 different customers per day (kids, elderlies, handicapped)</p>
+        </section>
+        <section className="jobContainer">
+            <h2>webförsterei</h2>
+            <p>11/18 - 06/19</p>
+            <p>Hamburg</p>
+            <p>Frontend-Developer as part of small teams with customers ranging from small local companies to big international companies</p>
+        </section>
     </div>);
 }
