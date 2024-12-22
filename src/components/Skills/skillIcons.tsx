e9c47ce5ea4821e028e5a7f5e7f467ccb009dc2c
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-12-22 19:39:34 +0100

    Refactored Skills component

---
+++ src/components/Skills/skillIcons.tsx
@@ -0,0 +1,87 @@
+//@ts-ignore
+import tsIcon from "../../assets/icons/ts.png";
+//@ts-ignore
+import jsIcon from "../../assets/icons/js.png";
+//@ts-ignore
+import htmlIcon from "../../assets/icons/html.png";
+//@ts-ignore
+import cssIcon from "../../assets/icons/css.png";
+//@ts-ignore
+import sassIcon from "../../assets/icons/sass.png";
+//@ts-ignore
+import expressIcon from "../../assets/icons/express.png";
+//@ts-ignore
+import restIcon from "../../assets/icons/rest.png";
+//@ts-ignore
+import mongoDBIcon from "../../assets/icons/mongoDB.png";
+//@ts-ignore
+import mongooseIcon from "../../assets/icons/mongoose.png";
+//@ts-ignore
+import noSqlIcon from "../../assets/icons/noSql.png";
+//@ts-ignore
+import sqlIcon from "../../assets/icons/sql.png";
+//@ts-ignore
+import awsIcon from "../../assets/icons/aws.png";
+//@ts-ignore
+import serverlessIcon from "../../assets/icons/serverless.png";
+//@ts-ignore
+import firebaseIcon from "../../assets/icons/firebase.png";
+//@ts-ignore
+import reactIcon from "../../assets/icons/react.svg";
+//@ts-ignore
+import angularIcon from "../../assets/icons/angular.png";
+//@ts-ignore
+import jqueryIcon from "../../assets/icons/jquery.png";
+//@ts-ignore
+import bootstrapIcon from "../../assets/icons/bootstrap.png";
+//@ts-ignore
+import nodejsIcon from "../../assets/icons/nodejs.png";
+//@ts-ignore
+import expoIcon from "../../assets/icons/expo.png";
+//@ts-ignore
+import puppeteerIcon from "../../assets/icons/puppeteer.png";
+//@ts-ignore
+import jestIcon from "../../assets/icons/jest.png";
+//@ts-ignore
+import cucumberIcon from "../../assets/icons/cucumber.png";
+//@ts-ignore
+import bddIcon from "../../assets/icons/bdd.png";
+//@ts-ignore
+import tddIcon from "../../assets/icons/tdd.png";
+//@ts-ignore
+import vscodeIcon from "../../assets/icons/vscode.png";
+//@ts-ignore
+import gitIcon from "../../assets/icons/git.png";
+//@ts-ignore
+import githubIcon from "../../assets/icons/github.png";
+
+export {
+    jsIcon,
+    tsIcon,
+    htmlIcon,
+    cssIcon,
+    sassIcon,
+    expressIcon,
+    restIcon,
+    mongoDBIcon,
+    mongooseIcon,
+    noSqlIcon,
+    sqlIcon,
+    awsIcon,
+    serverlessIcon,
+    firebaseIcon,
+    reactIcon,
+    angularIcon,
+    jqueryIcon,
+    bootstrapIcon,
+    nodejsIcon,
+    expoIcon,
+    puppeteerIcon,
+    jestIcon,
+    cucumberIcon,
+    bddIcon,
+    tddIcon,
+    vscodeIcon,
+    gitIcon,
+    githubIcon
+};
--- src/components/Skills/skills.tsx
+++ src/components/Skills/skills.tsx
@@ -1,77 +1,144 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import "./skills.scss";
+import {
+    jsIcon,
+    tsIcon,
+    htmlIcon,
+    cssIcon,
+    sassIcon,
+    expressIcon,
+    restIcon,
+    mongoDBIcon,
+    mongooseIcon,
+    noSqlIcon,
+    sqlIcon,
+    awsIcon,
+    serverlessIcon,
+    firebaseIcon,
+    reactIcon,
+    angularIcon,
+    jqueryIcon,
+    bootstrapIcon,
+    nodejsIcon,
+    expoIcon,
+    puppeteerIcon,
+    jestIcon,
+    cucumberIcon,
+    bddIcon,
+    tddIcon,
+    vscodeIcon,
+    gitIcon,
+    githubIcon
+} from "./skillIcons.tsx";
 
 export default function Skills() {
-    const header = useRef<HTMLHeadingElement>(null);
+    const skillSections: {
+        'Frontend-Developer': { icon: string, name: string }[],
+        'Backend-Developer': { icon: string, name: string }[],
+        'Framework-Fan': { icon: string, name: string }[],
+        'Mobile Developer': { icon: string, name: string }[],
+        'Code-Tester': { icon: string, name: string }[],
+        'Tool-Guru': { icon: string, name: string }[]
+    } = {
+        'Frontend-Developer': [
+            { icon: jsIcon, name: "JavaScript" },
+            { icon: tsIcon, name: "TypeScript" },
+            { icon: htmlIcon, name: "HTML" },
+            { icon: cssIcon, name: "CSS" },
+            { icon: sassIcon, name: "Sass" }
+        ],
+        'Backend-Developer': [
+            { icon: expressIcon, name: "Express.js" },
+            { icon: restIcon, name: "RESTful" },
+            { icon: mongoDBIcon, name: "MongoDB" },
+            { icon: mongooseIcon, name: "Mongoose" },
+            { icon: noSqlIcon, name: "noSQL" },
+            { icon: sqlIcon, name: "SQL" },
+            { icon: awsIcon, name: "AWS" },
+            { icon: serverlessIcon, name: "Serverless" },
+            { icon: firebaseIcon, name: "Google Firebase" }
+        ],
+        'Framework-Fan': [
+            { icon: reactIcon, name: "React" },
+            { icon: angularIcon, name: "Angular" },
+            { icon: jqueryIcon, name: "jQuery" },
+            { icon: bootstrapIcon, name: "Bootstrap" },
+            { icon: nodejsIcon, name: "Node.js" }
+        ],
+        'Mobile Developer': [
+            { icon: reactIcon, name: "React Native" },
+            { icon: expoIcon, name: "Expo" }
+        ],
+        'Code-Tester': [
+            { icon: puppeteerIcon, name: "Puppeteer" },
+            { icon: jestIcon, name: "Jest" },
+            { icon: cucumberIcon, name: "Cucumber/Gherkin" },
+            { icon: bddIcon, name: "Behavior-driven Development" },
+            { icon: tddIcon, name: "Test-driven Development" }
+        ],
+        'Tool-Guru': [
+            { icon: vscodeIcon, name: "Visual Studio Code" },
+            { icon: gitIcon, name: "Git" },
+            { icon: githubIcon, name: "GitHub" }
+        ]
+    };
     useEffect(() => {
         const handleScroll = () => {
-            if (header.current) {
-                if ((header.current.getBoundingClientRect()).top < 1) {
-                    header.current.style.fontSize = "1.5rem";
-                } else {
-                    header.current.style.fontSize = "2.5rem";
+            const [skillFigures, skillHeadings, skillListElements, [iamaHeader]] = [
+                "ol.skills>li>section>div>figure",
+                "ol.skills>li>h2",
+                "ol.skills>li",
+                "div.skillContainer>h1"
+            ].map((selector) => document.querySelectorAll<HTMLElement>(selector));
+            const { top } = iamaHeader.getBoundingClientRect();
+            iamaHeader.style.fontSize = top < 1 ? "1.5rem" : "2.5rem";
+            iamaHeader.style.opacity = skillListElements[skillListElements.length - 1].getBoundingClientRect().top < 61 ? '0' : `${Math.min(1, Math.max(0, (window.innerHeight - top) / (window.innerHeight / 3)))}`;
+            for (let skillParts of [skillFigures, skillHeadings, skillListElements]) {
+                for (let skillPart of skillParts) {
+                    const { top, bottom } = skillPart.getBoundingClientRect();
+                    skillPart.style.opacity = `${Math.min(1, Math.max(0, (window.innerHeight - top) / (window.innerHeight / 3)), Math.max(0, bottom / (window.innerHeight / 3)))}`;
                 }
             }
         };
-
         window.addEventListener("scroll", handleScroll);
-
         return () => {
             window.removeEventListener("scroll", handleScroll);
         };
     }, []);
+
     return (
-        <div className="skillsContainer">
-            <h1 ref={header}>What I offer</h1>
-            <h2>Frontend-Development</h2>
-            <span>JavaScript,</span>
-            <span>TypeScript,</span>
-            <span>HTM,L</span>
-            <span>CSS,</span>
-            <span>SASS</span>
-            <h2>Backend-Development</h2>
-            <span>Express.js,</span>
-            <span>RESTful,</span>
-            <span>MongoDB,</span>
-            <span>Mongoose,</span>
-            <span>NoSQL,</span>
-            <span>SQL,</span>
-            <span>Amazon Web Services,</span>
-            <span>Serverless,</span>
-            <span>Google Firebase</span>
-            <h2>Frameworks/Libraries</h2>
-            <span>React,</span>
-            <span>Angular,</span>
-            <span>jQuery,</span>
-            <span>Bootstrap</span>
-            <h2>Mobile Development</h2>
-            <span>React Native,</span>
-            <span>Expo</span>
-            <h2>Testing</h2>
-            <span>Puppeteer,</span>
-            <span>Jest,</span>
-            <span>Cucumber/Gherkin,</span>
-            <span>Behavior-/Test-driven Development</span>
-            <h2>Tools</h2>
-            <span>Visual Studio Code,</span>
-            <span>Git,</span>
-            <span>GitHub,</span>
-            <span>GitHub Copilot,</span>
-            <span>Postman,</span>
-            <span>Node.js,</span>
-            <span>Chrome Developer Tools</span>
-            <div id="education">
-                <h2>Education</h2>
-                <span>10/13 - 09/19</span>
-                <span>Software-System-Development</span>
-                <span>University of Hamburg</span>
-            </div>
-            <div id="languages">
-                <h2>Languages</h2>
-                <span>German (native),</span>
-                <span>English (fluent),</span>
-                <span>Spanish (basic)</span>
-            </div>
+        <div className="skillContainer">
+            <h1>I'm a</h1>
+            <ol className="skills" style={{ '--length': 6 } as React.CSSProperties}>
+                {(Object.keys(skillSections)).map((skillSectionName, i) => (
+                    <li key={skillSectionName} style={{ '--i': i + 1 } as React.CSSProperties}>
+                        <h2>{skillSectionName}</h2>
+                        <section className="overflowX">
+                            <div className="horizontalSkillsList">
+                                {skillSections[skillSectionName].map(({ icon, name }) => (
+                                    <figure key={name}>
+                                        <picture><img src={icon} alt={name + ' icon'} /></picture>
+                                        <figcaption>{name}</figcaption>
+                                    </figure>
+                                ))}
+                            </div>
+                        </section>
+                    </li>
+                ))}
+            </ol>
         </div>
+        //     <div id="education">
+        //         <h2>Education</h2>
+        //         <span>10/13 - 09/19</span>
+        //         <span>Software-System-Development</span>
+        //         <span>University of Hamburg</span>
+        //     </div>
+        //     <div id="languages">
+        //         <h2>Languages</h2>
+        //         <span>German (native),</span>
+        //         <span>English (fluent),</span>
+        //         <span>Spanish (basic)</span>
+        //     </div>
+        // </div>
     );
 }
